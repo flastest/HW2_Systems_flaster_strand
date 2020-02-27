b8f0032be82e88460605a577f32663632ecec6da
flastest <pleasantbowel5yndrome@gmail.com>
2020-02-27 12:35:44 -0800

    adding the impl

--- cache.cc
+++ cache.cc
@@ -1,9 +1,33 @@
 #include <cache.hh>
 
+
+Cache::Impl 
+{
+private:
+	size_type mMaxmem;
+	float mMax_load_factor;
+	Evictor* mEvictor;
+	hash_func mHasher;
+
+public:
+	Impl(size_type maxmem,
+        float max_load_factor = 0.75,
+        Evictor* evictor = nullptr,
+        hash_func hasher = std::hash<key_type>())
+	mMaxmem : maxmem, mMax_load_factor: max_load_factor,
+	mEvictor : evictor, mHasher : hasher
+	{}
+
+
+}
+
 Cache::Cache(size_type maxmem,
         float max_load_factor = 0.75,
         Evictor* evictor = nullptr,
         hash_func hasher = std::hash<key_type>())
 {
-	
+
 }
+
+
+Cache::~Cache();
