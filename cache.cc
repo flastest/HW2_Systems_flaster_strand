e0b3d56eee1f649c16c8fcd2b162445acf905024
flastest <pleasantbowel5yndrome@gmail.com>
2020-03-01 12:44:32 -0800

    I drank coffee this mmorning

--- cache.cc
+++ cache.cc
@@ -1,42 +1,68 @@
 #include "cache.hh"
 
 
+
 class Cache::Impl
 {
 private:
-	size_type mMaxmem;
+	Cache::size_type mMaxmem;
 	float mMax_load_factor;
 	Evictor* mEvictor;
 	hash_func mHasher;
 
+	//the cache should look through the list of keys to see if that key is
+	//in the array
+	std::vector<key_type> keys;
+	std::unique_ptr<Cache::val_type> data;
+
+	//for the trivial impplementation, the cache will stop adding things when
+	// it gets full
+	bool is_full = false;
+
+
+
 public:
-	Impl(size_type maxmem,
+	Impl(Cache::size_type maxmem,
         float max_load_factor,
         Evictor* evictor,
-        hash_func hasher)
-	mMaxmem : maxmem, mMax_load_factor: max_load_factor,
-	mEvictor : evictor, mHasher : hasher
+        hash_func hasher) :
+	mMaxmem(maxmem), mMax_load_factor(max_load_factor),
+	mEvictor(evictor), mHasher(hasher)
+	{
+		data = new Cache::val_type[mMaxmem](); 
+							//here, the parens on the end initialize
+							// all of the things in data to zeros.
+	}
+
+	//uses mHasher function to choose whether or not to encache something
+	void hash(key_type key, Cache::val_type val, Cache::size_type size)
+	{
+		//evictor: 
+		// if cache is full, just don't add to the cache
+
+		//to determine where to put in the cache, 
+
+	}
+};
+
+Cache::Cache(Cache::size_type maxmem,
+        float max_load_factor,
+        Evictor* evictor,
+        hash_func hasher):
+	pImpl_ (new Impl(maxmem,max_load_factor,evictor,hasher))
 	{}
 
 
-}
 
-Cache::Cache(size_type maxmem,
-        float max_load_factor,
-        Evictor* evictor,
-        hash_func hasher)
+void set(key_type key, Cache::val_type val, Cache::size_type size)
 {
-	pImpl_ = new Impl(maxmem,max_load_factor,evictor,hasher);
-}
-
-
-void set(key_type key, val_type val, size_type size)
-{
+	//finds a space in data where we can put things. I want to use a ring 
+	//buffer because I think they are OP
 
 
 }
 
-val_type get(key_type key, size_type& val_size) const 
+Cache::val_type get(key_type key, Cache::size_type& Cache::val_size) const 
 {
 	return nullptr;
 }
@@ -46,7 +72,7 @@ bool del(key_type key)
 	return true;
 }
 
-size_type space_used() const 
+Cache::size_type space_used() const 
 {
 	return static_cast<uint32_t>(966);
 }
--- test_cache.cc
+++ test_cache.cc
@@ -1,5 +1,10 @@
 #include "cache.hh"
 
+//include a tester for a hasher
+//this hasher just returns the hash given from the key value mod size of cache.
+class hash
+
+
 
 int test_construct(){
     return 0;
